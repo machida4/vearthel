3f672443179181cd7c58e5f515a8ff921e4ed800
machida4 <machida@kmc.gr.jp>
2023-09-18 00:16:26 +0900

    h1 tag

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,12 @@
 import { NextPage } from 'next';
 
 const AboutPage: NextPage = async () => {
-  return <div className="content">About me ...</div>;
+  return (
+    <div>
+      <h1>About</h1>
+      <h2>Machida4</h2>
+    </div>
+  );
 };
 
 export default AboutPage;
