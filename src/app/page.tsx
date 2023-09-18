f94b787f6859eda3cddfcf84a4f14e8981f0cc00
machida <machida@kmc.gr.jp>
2023-09-18 15:29:44 +0900

    react-three-fiber導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,43 @@
+import { Canvas, useFrame } from '@react-three/fiber';
 import { NextPage } from 'next';
+import { useRef, useState } from 'react';
+
+function Box(props) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef();
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.x += delta));
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => (event.stopPropagation(), hover(true))}
+      onPointerOut={(event) => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  );
+}
 
 const Home: NextPage = async () => {
-  return <div className="content">Hello, World!</div>;
+  // return <div className="content">Hello, World!</div>;
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+      {/* <OrbitControls /> */}
+    </Canvas>
+  );
 };
 
 export default Home;
