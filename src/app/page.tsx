b7a783a7c56cb3d66098a499fa4bda9fcce4d514
machida <machida@kmc.gr.jp>
2023-09-18 18:31:04 +0900

    globeを表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,34 @@
-import { Canvas, useFrame } from '@react-three/fiber';
+'use client';
+
+import { OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
 import { NextPage } from 'next';
-import { useRef, useState } from 'react';
+import React, { Suspense } from 'react';
+import Globe from './_components/Globe';
 
-function Box(props) {
-  // This reference gives us direct access to the THREE.Mesh object
-  const ref = useRef();
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false);
-  const [clicked, click] = useState(false);
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += delta));
-  // Return the view, these are regular Threejs elements expressed in JSX
+const Scene: React.FC = () => {
   return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => (event.stopPropagation(), hover(true))}
-      onPointerOut={(event) => hover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
+    <div className="mx-auto">
+      <Canvas className="relative h-full">
+        <Suspense fallback={null}>
+          <ambientLight intensity={2.0} />
+          {/* <directionalLight color="red" position={[0, 0, 5]} intensity={0.5} /> */}
+          {/* <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+          <pointLight position={[-10, -10, -10]} /> */}
+          <Globe route={'/about'} />
+          <OrbitControls enablePan={false} enableZoom={false} />
+        </Suspense>
+      </Canvas>
+    </div>
   );
-}
+};
 
-const Home: NextPage = async () => {
-  // return <div className="content">Hello, World!</div>;
+const Home: NextPage = () => {
   return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-      {/* <OrbitControls /> */}
-    </Canvas>
+    <>
+      <div>Hello, World!</div>
+      <Scene />
+    </>
   );
 };
 
