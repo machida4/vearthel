6609b07c7a0e90bb8ff1047c2a059d0bdd258038
machida <machida@kmc.gr.jp>
2023-09-18 18:31:14 +0900

    addわすれてた

---
+++ src/app/_components/Globe.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { useCursor } from '@react-three/drei';
+import { useFrame, useLoader } from '@react-three/fiber';
+import { useRouter } from 'next/navigation';
+import { useRef, useState } from 'react';
+import { Mesh, TextureLoader } from 'three';
+
+type Props = {
+  route: string;
+};
+
+const Globe: React.FC<Props> = ({ route }) => {
+  const meshRef = useRef<Mesh>(null!);
+  const router = useRouter();
+
+  const [hovered, hover] = useState(false);
+  const globeMap = useLoader(TextureLoader, 'icon.png');
+
+  useCursor(hovered);
+  useFrame((_, delta) => (meshRef.current.rotation.y += delta / 2));
+
+  return (
+    <mesh
+      ref={meshRef}
+      onClick={() => router.push(route)}
+      onPointerOver={() => hover(true)}
+      onPointerOut={() => hover(false)}
+    >
+      <sphereGeometry args={[1, 64, 64]} />
+      <meshStandardMaterial map={globeMap} />
+    </mesh>
+  );
+};
+
+export default Globe;
